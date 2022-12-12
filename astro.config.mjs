24ae7787b0f1083f2c26130fdc75054bc55a5dc5
clayton-schneider <schneiderc2016@gmail.com>
2022-12-12 11:03:37 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://sitegoeshere.com",
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    image(),
+    sitemap(),
+    partytown(),
+  ],
+});
