027b4867ce3e38f7be1304f78065e7933ff13656
clayton-schneider <schneiderc2016@gmail.com>
2022-12-23 10:15:21 -0500

    header responsive, need to change mobile menu color

--- src/components/Header/NavMenu.tsx
+++ src/components/Header/NavMenu.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Hamburger from "./Hamburger";
 import PrimaryBtn from "@components/buttons/PrimaryBtn";
+import SecondaryBtn from "@components/buttons/SecondaryBtn";
 
 interface Props {
   links: {
@@ -20,13 +21,11 @@ const NavMenu = ({ links }: Props) => {
   };
 
   return (
-    <div className="my-5 py-5  border-b border-white px-12 flex justify-between">
+    <div className="xl:my-5 xl:py-5  xl:border-b xl:border-white xl:px-12 flex  justify-between">
       <ul
         className={
           "absolute top-[144px] z-50 flex w-full flex-col items-start gap-6 bg-white font-bebas py-5 px-4 text-xl transition-all duration-300 xl:static xl:w-auto xl:flex-row xl:items-center xl:bg-transparent xl:py-0 xl:px-0 " +
-          (isOpen
-            ? "left-0 z-40 !text-black shadow shadow-neutral-300"
-            : "-left-full text-white")
+          (isOpen ? "left-0 z-40 !text-black" : "-left-full text-white")
         }
       >
         {links.map((link, idx) => (
@@ -34,14 +33,22 @@ const NavMenu = ({ links }: Props) => {
             <a href={link.link}>{link.linkText}</a>
           </li>
         ))}
-        {/* <PrimaryBtn
-          to="/request-a-quote"
-          className="!block w-full text-center lg:text-left lg:w-auto"
+        <PrimaryBtn
+          to="/"
+          className="!block xl:!hidden w-full text-center lg:text-left lg:w-auto"
         >
-          Join Us
-        </PrimaryBtn> */}
+          Client Portal
+        </PrimaryBtn>
+        <SecondaryBtn
+          to="/"
+          className="!block xl:!hidden w-full text-center lg:text-left lg:w-auto"
+        >
+          Pay My Bill
+        </SecondaryBtn>
       </ul>
-      <p className="font-bold text-xl text-text-light">479-273-2539</p>
+      <p className="hidden xl:block font-bold text-xl text-text-light">
+        479-273-2539
+      </p>
       <Hamburger clicked={handleClick} isOpen={isOpen} color="white" />
     </div>
   );
