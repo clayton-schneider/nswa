ff13d13d385868825cde855f7a5b8be072efb173
clayton-schneider <schneiderc2016@gmail.com>
2024-03-01 15:33:22 -0500

    partytown and plausible

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -10,20 +9,19 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://www.nswadvisors.com/",
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-    react(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap(), react(), partytown()],
   vite: {
     ssr: {
-      noExternal: ["@splidejs/splide"],
-    },
-  },
+      noExternal: ["@splidejs/splide"]
+    }
+  }
 });
